8ea7891be029e45a3df3cbf4409c595a23b76855
renu.bansal <renu.bansal@thoughtworks.com>
2019-11-13 20:44:24 +0530

    add form builder

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { CartService} from '../cart.service'
+import {Component, OnInit} from '@angular/core';
+import {CartService} from '../cart.service'
+import {FormBuilder} from "@angular/forms";
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+    selector: 'app-cart',
+    templateUrl: './cart.component.html',
+    styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
 
-  items;
-  constructor(
-      private cartService : CartService
-  ) { }
+    items;
+    checkoutForm;
 
-  ngOnInit() {
-    this.items = this.cartService.getItems();
-  }
+    constructor(
+        private cartService: CartService,
+        private formBuilder: FormBuilder,
+    ) {
+    }
+
+    ngOnInit() {
+        this.items = this.cartService.getItems();
+        this.checkoutForm = this.formBuilder.group(
+            {
+                name: '',
+                address: ''
+            }
+        );
+    }
+
+    onSubmit(customerData){
+      console.warn('Your order has been submitted' ,customerData)
+      this.cartService.clearCart();
+      this.checkoutForm.reset();
+    }
 
 }
