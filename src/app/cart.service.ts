8bfef123c8d0d75dfe51d5fa824430d41435e9fa
renu.bansal <renu.bansal@thoughtworks.com>
2019-11-13 20:30:41 +0530

    add shipping component to display the prices

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items = [];
-  constructor() { }
+  constructor(
+      private http : HttpClient
+  ) { }
 
   addToCart(product) {
     this.items.push(product);
@@ -18,6 +21,10 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices(){
+    return this.http.get('/assets/shipping.json');
   }
 
 }
